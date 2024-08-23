c0ad691eae86fe9797a86f5d1d023c577ba84c81
eunsikim <kimeunsik229@gmail.com>
2024-08-22 20:28:54 -0400

    ExampleA.java

--- Week-2/ExampleA.java
+++ Week-2/ExampleA.java
@@ -5,7 +5,29 @@ public class ExampleA {
         return res;
     }
 
+    public static char[] reverse(char[] original){
+        char[] reversed = new char[original.length];
+
+        // Processing
+        for(int i = 0, j = original.length - 1; i < reversed.length; i++, j--){
+            reversed[i] = original[j];
+        }
+
+        return reversed;
+    }
+
     public static void main(String[] args) {
-        
+        char[] array = getArray();
+
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+        }
+
+        System.out.println();
+
+        char[] arrayReversed = reverse(array);
+        for(int i = 0; i < arrayReversed.length; i++){
+            System.out.print(arrayReversed[i]);
+        }
     }
 }
