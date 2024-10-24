03453b621ca11188f38f9df6e882a19b0bcd06d3
Kim Eun Sik <kimeunsik229@gmail.com>
2024-10-24 23:46:50 +0000

    File Reading/Writing example using BufferedReader and BufferedWriter

---
+++ Week-10/ExampleB.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExampleB {
+    public static void main(String[] args) {
+        final String FILE_PATH = "./TODO.TXT"; // Whichever the path to your file is.
+
+        // try (BufferedWriter wr = new BufferedWriter(new FileWriter(FILE_PATH, true))) { // append to text file
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(FILE_PATH))) { // Overwrite text file contents
+            wr.write("4. Work on Assignment 6\n");
+            wr.write("5. Work on Lab 6\n");
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+    }
+}
